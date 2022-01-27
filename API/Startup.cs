4a376a50b542b24fc62dc087c48a3acb7fc51d52
bonitowoo <bonitowoo@gmail.com>
2022-01-26 16:46:54 -0800

    end of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -35,6 +35,7 @@ namespace API
             });
 
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
@@ -55,7 +56,11 @@ namespace API
 
             app.UseRouting();
 
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
             app.UseAuthorization();
+
+
 
             app.UseEndpoints(endpoints =>
             {
